01cb32b2baf41bdfdabda85755e2671bb409ebba
MarcoMadera <me@marcomadera.com>
2021-09-26 01:18:40 -0500

    router in effect

--- pages/collection/index.tsx
+++ pages/collection/index.tsx
@@ -1,9 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
 export default function Collection(): ReactElement {
   const router = useRouter();
-  router.push("/collection/playlists");
+  useEffect(() => {
+    router.push("/collection/playlists");
+  }, [router]);
   return <Head>Rindu - Library</Head>;
 }
