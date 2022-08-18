41af8683ecf2c49285f1917e6fcd4ae01e2842d4
MarcoMadera <marcoseguro2@gmail.com>
2022-08-17 17:41:14 -0700

    add some tests

---
+++ components/__tests__/CardTrack.spec.tsx
@@ -0,0 +1,279 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import CardTrack from "components/CardTrack";
+import { IUtilsMocks } from "types/mocks";
+import { AppProviders } from "./mocks";
+import useOnScreen from "hooks/useOnScreen";
+import { playCurrentTrack } from "utils/playCurrentTrack";
+import type { IToast } from "types/toast";
+
+const { track } = jest.requireActual<IUtilsMocks>(
+  "utils/__tests__/__mocks__/mocks.ts"
+);
+jest.mock("hooks/useOnScreen");
+jest.mock("utils/playCurrentTrack");
+
+describe("cardTrack", () => {
+  const onClickAdd = jest.fn();
+  it("renders", () => {
+    expect.assertions(1);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    const cardTrackName = {
+      ...track,
+      name: "cardTrackName",
+    };
+
+    render(
+      <AppProviders>
+        <CardTrack
+          track={cardTrackName}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{ margin: "50px" }}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const titleValue = screen.getByText(cardTrackName.name);
+    expect(titleValue).toHaveTextContent(cardTrackName.name);
+  });
+
+  it("should play on double click", async () => {
+    expect.assertions(2);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    (playCurrentTrack as jest.Mock<Promise<number>>).mockResolvedValue(200);
+    const setPlayedSource = jest.fn();
+
+    render(
+      <AppProviders value={{ setPlayedSource }}>
+        <CardTrack
+          track={track}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const mytest = screen.getByTestId("cardTrack-container");
+    fireEvent.doubleClick(mytest);
+    await waitFor(() => {
+      expect(setPlayedSource).toHaveBeenCalledWith(track.uri);
+    });
+  });
+  it("should add toast reconnection error", async () => {
+    expect.assertions(4);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    (playCurrentTrack as jest.Mock<Promise<number>>).mockResolvedValue(404);
+    let toasts: IToast[] = [];
+    const player = {
+      disconnect: jest.fn(),
+    } as unknown as Spotify.Player;
+    const setReconnectionError = jest.fn();
+    const setToasts = (toastArray: IToast[] | ((toasts: IToast[]) => void)) => {
+      // eslint-disable-next-line jest/no-conditional-in-test
+      if (typeof toastArray === "function") {
+        toastArray(toasts);
+      } else {
+        toasts = toastArray;
+      }
+    };
+
+    render(
+      <AppProviders value={{ toasts, setToasts, player, setReconnectionError }}>
+        <CardTrack
+          track={track}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const mytest = screen.getByTestId("cardTrack-container");
+    fireEvent.doubleClick(mytest);
+    await waitFor(() => {
+      expect(player.disconnect).toHaveBeenCalledWith();
+    });
+    expect(setReconnectionError).toHaveBeenCalledWith(true);
+    expect(toasts).toStrictEqual([
+      {
+        message: "Unable to play, trying to reconnect, please wait...",
+        displayTime: 10000,
+        id: expect.any(String),
+        timeOut: expect.any(Number),
+        variant: "error",
+      },
+    ]);
+  });
+  it("should add toast with error playing this track", async () => {
+    expect.assertions(2);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    (playCurrentTrack as jest.Mock<Promise<number>>).mockResolvedValue(400);
+    let toasts: IToast[] = [];
+    const setReconnectionError = jest.fn();
+    const setToasts = (toastArray: IToast[] | ((toasts: IToast[]) => void)) => {
+      // eslint-disable-next-line jest/no-conditional-in-test
+      if (typeof toastArray === "function") {
+        toastArray(toasts);
+      } else {
+        toasts = toastArray;
+      }
+    };
+
+    render(
+      <AppProviders value={{ toasts, setToasts, setReconnectionError }}>
+        <CardTrack
+          track={track}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const mytest = screen.getByTestId("cardTrack-container");
+    fireEvent.doubleClick(mytest);
+    await waitFor(() => {
+      expect(toasts).toStrictEqual([
+        {
+          message: "Error playing this track",
+          displayTime: 10000,
+          id: expect.any(String),
+          timeOut: expect.any(Number),
+          variant: "error",
+        },
+      ]);
+    });
+  });
+  it("should add toast with error if is corrupted track", async () => {
+    expect.assertions(1);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    let toasts: IToast[] = [];
+    const setReconnectionError = jest.fn();
+    const setToasts = (toastArray: IToast[] | ((toasts: IToast[]) => void)) => {
+      // eslint-disable-next-line jest/no-conditional-in-test
+      if (typeof toastArray === "function") {
+        toastArray(toasts);
+      } else {
+        toasts = toastArray;
+      }
+    };
+
+    render(
+      <AppProviders value={{ toasts, setToasts, setReconnectionError }}>
+        <CardTrack
+          track={{ ...track, corruptedTrack: true }}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const mytest = screen.getByTestId("cardTrack-container");
+    fireEvent.doubleClick(mytest);
+    expect(toasts).toStrictEqual([
+      {
+        message: "This track is corrupted and cannot be played",
+        displayTime: 10000,
+        id: expect.any(String),
+        timeOut: expect.any(Number),
+        variant: "error",
+      },
+    ]);
+  });
+  it("should add toast with info no content available", async () => {
+    expect.assertions(1);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+    let toasts: IToast[] = [];
+    const setReconnectionError = jest.fn();
+    const setToasts = (toastArray: IToast[] | ((toasts: IToast[]) => void)) => {
+      // eslint-disable-next-line jest/no-conditional-in-test
+      if (typeof toastArray === "function") {
+        toastArray(toasts);
+      } else {
+        toasts = toastArray;
+      }
+    };
+
+    render(
+      <AppProviders value={{ toasts, setToasts, setReconnectionError }}>
+        <CardTrack
+          track={{ ...track, is_playable: false }}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const mytest = screen.getByTestId("cardTrack-container");
+    fireEvent.doubleClick(mytest);
+    expect(toasts).toStrictEqual([
+      {
+        message: "This content is not available",
+        displayTime: 10000,
+        id: expect.any(String),
+        timeOut: expect.any(Number),
+        variant: "info",
+      },
+    ]);
+  });
+  it("should change artists styles on mouse enter", async () => {
+    expect.assertions(1);
+    (useOnScreen as jest.Mock).mockImplementationOnce(() => true);
+
+    render(
+      <AppProviders>
+        <CardTrack
+          track={{
+            ...track,
+            artists: [{ name: "Artist name", id: "artistId" }],
+          }}
+          type="presentation"
+          position={0}
+          accessToken=""
+          style={{}}
+          isTrackInLibrary={false}
+          isSingleTrack={true}
+          playlistUri=""
+          uri=""
+          onClickAdd={onClickAdd}
+        />
+      </AppProviders>
+    );
+    const link = screen.getByText("Artist name");
+    fireEvent.mouseEnter(link);
+    expect(link).toHaveStyle("color: #fff");
+  });
+});
---
+++ components/__tests__/mocks.tsx
@@ -0,0 +1,111 @@
+import ContextMenu from "components/ContextMenu";
+import ContextMenuContext from "context/ContextMenuContext";
+import HeaderContext, { IHeaderContext } from "context/HeaderContext";
+import SpotifyContext from "context/SpotifyContext";
+import ToastContext, { ToastContextProviderProps } from "context/ToastContext";
+import UserContext, { IUserContext } from "context/UserContext";
+import { PropsWithChildren, ReactElement } from "react";
+import { ContextMenuContextProviderProps } from "types/contextMenu";
+import { ISpotifyContext } from "types/spotify";
+
+// eslint-disable-next-line jest/no-mocks-import
+import {
+  trackFull,
+  user,
+  accessToken,
+  track,
+  simplePlaylist,
+} from "utils/__tests__/__mocks__/mocks";
+
+type AppContext = IUserContext &
+  ISpotifyContext &
+  IHeaderContext &
+  ToastContextProviderProps;
+
+interface ContextValues {
+  value: Partial<AppContext>;
+}
+
+export const AppProviders = ({
+  children,
+  value,
+}: PropsWithChildren<Partial<ContextValues>>): ReactElement => {
+  return (
+    <div id="root">
+      <div id="tracksModal" />
+      <div id="toast" />
+      <div id="contextMenu" />
+      <ToastContext.Provider
+        value={{ toasts: [], ...value } as ToastContextProviderProps}
+      >
+        <UserContext.Provider
+          value={
+            {
+              user: user,
+              accessToken,
+              isLogin: true,
+              ...value,
+            } as IUserContext
+          }
+        >
+          <HeaderContext.Provider
+            value={
+              {
+                alwaysDisplayColor: true,
+                headerColor: "#ccc",
+                headerOpacity: 1,
+                element: null,
+                disableBackground: true,
+                disableOpacityChange: true,
+                displayOnFixed: true,
+                ...value,
+              } as IHeaderContext
+            }
+          >
+            <SpotifyContext.Provider
+              value={
+                {
+                  deviceId: "deviceId",
+                  playlists: [simplePlaylist],
+                  allTracks: [trackFull],
+                  currrentlyPlaying: track,
+                  isPlaying: false,
+                  currentlyPlayingDuration: 4000,
+                  currentlyPlayingPosition: 30,
+                  isPip: false,
+                  isShowingSideBarImg: true,
+                  lastVolume: 0.5,
+                  pageDetails: {},
+                  playedSource: "",
+                  totalPlaylists: 1,
+                  player: {
+                    disconnect: () => {
+                      console.log("disconnect");
+                    },
+                  },
+                  ...value,
+                } as ISpotifyContext
+              }
+            >
+              <ContextMenuContext.Provider
+                value={
+                  {
+                    contextMenuData: {
+                      data: track,
+                      position: { x: 0, y: 0 },
+                      type: "cardTrack",
+                    },
+                    ...value,
+                  } as ContextMenuContextProviderProps
+                }
+              >
+                <ContextMenu />
+                {children}
+              </ContextMenuContext.Provider>
+            </SpotifyContext.Provider>
+          </HeaderContext.Provider>
+        </UserContext.Provider>
+      </ToastContext.Provider>
+    </div>
+  );
+};
