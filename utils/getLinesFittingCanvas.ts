4272b87a23e8d04e990ba184542209654d311bdd
MarcoMadera <marcoseguro2@gmail.com>
2022-10-27 11:41:25 -0700

    fix lines that were too long were coming off the canvas

---
+++ utils/getLinesFittingCanvas.ts
@@ -0,0 +1,22 @@
+export function getLinesFittingCanvas(
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  maxWidth: number
+): string[] {
+  const words = text.split(" ");
+  const lines = [];
+  let currentLine = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    const word = words[i];
+    const width = ctx.measureText(currentLine + " " + word).width;
+    if (width < maxWidth) {
+      currentLine += " " + word;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
+    }
+  }
+  lines.push(currentLine);
+  return lines;
+}
