46ad3676cef47db1e2b5c43c1a6b1e54d841940b
MarcoMadera <marcoseguro2@gmail.com>
2023-07-10 14:52:51 -0700

    add 2 tests

---
+++ components/__tests__/ContentContainer.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+
+import ContentContainer from "components/ContentContainer";
+
+describe("contentContainer", () => {
+  it("renders main container with children", () => {
+    expect.assertions(1);
+    render(<ContentContainer>Test</ContentContainer>);
+
+    expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+
+  it("test_render_main_container_without_children", () => {
+    expect.assertions(1);
+    render(<ContentContainer />);
+
+    expect(screen.queryByText("Test")).not.toBeInTheDocument();
+  });
+
+  it("test_render_main_container_with_has_page_header_true", () => {
+    expect.assertions(1);
+    render(<ContentContainer hasPageHeader={true}>Test</ContentContainer>);
+
+    expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+});
