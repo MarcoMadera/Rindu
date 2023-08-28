5919d67c3d38e2d44b42c07fa9a7af09dee2884f
Marco Madera <me@marcomadera.com>
2023-08-27 18:17:48 -0700

    lyrics calculations (#207)

--- utils/getLinesFittingCanvas.ts
+++ utils/getLinesFittingCanvas.ts
@@ -1,3 +1,5 @@
+import { IFormatLyricsResponse } from "./formatLyrics";
+
 export function getLinesFittingCanvas(
   ctx: CanvasRenderingContext2D,
   text: string,
@@ -19,4 +21,126 @@ export function getLinesFittingCanvas(
   }
   lines.push(currentLine);
   return lines;
+}
+
+interface IGetLineInfo {
+  currentLine: IFormatLyricsResponse["lines"][0];
+  nextLine: IFormatLyricsResponse["lines"][0];
+  lyricLineColor: string;
+  lyricTextColor: string;
+  lyricsProgressMs: number;
+}
+interface IGetLineType {
+  currentLine: IFormatLyricsResponse["lines"][0];
+  nextLine: IFormatLyricsResponse["lines"][0];
+  lyricsProgressMs: number;
+}
+
+export function getLineType({
+  currentLine,
+  lyricsProgressMs,
+  nextLine,
+}: IGetLineType): "current" | "previous" | "next" {
+  const isPreviousLine =
+    currentLine.startTimeMs &&
+    Number(currentLine.startTimeMs) <= lyricsProgressMs;
+  const isNextLine =
+    nextLine?.startTimeMs && Number(nextLine?.startTimeMs) >= lyricsProgressMs;
+
+  const isCurrentLine = isPreviousLine && isNextLine;
+
+  const lineTypes = {
+    current: isCurrentLine,
+    previous: isPreviousLine,
+    next: isNextLine,
+  };
+
+  const type = Object.keys(lineTypes).find(
+    (key) => lineTypes[key as keyof typeof lineTypes]
+  ) as keyof typeof lineTypes;
+
+  return type;
+}
+
+export function getLineInfo({
+  currentLine,
+  lyricLineColor,
+  lyricTextColor,
+  lyricsProgressMs,
+  nextLine,
+}: IGetLineInfo): {
+  color: string;
+  text: string;
+  type: "current" | "previous" | "next";
+} {
+  const type = getLineType({
+    currentLine,
+    lyricsProgressMs,
+    nextLine,
+  });
+
+  const lineColors = {
+    current: "#fff",
+    previous: lyricLineColor + "80",
+    next: lyricTextColor,
+  };
+
+  return {
+    color: lineColors[type],
+    text: currentLine.words,
+    type,
+  };
+}
+
+interface IgetAllLinesFittingWidth {
+  lines: IFormatLyricsResponse["lines"];
+  ctx: CanvasRenderingContext2D;
+  lyricLineColor: string;
+  lyricTextColor: string;
+  lyricsProgressMs: number;
+  canvasWidth?: number;
+}
+
+export function getAllLinesFittingWidth({
+  ctx,
+  lines,
+  lyricLineColor,
+  lyricTextColor,
+  lyricsProgressMs,
+  canvasWidth,
+}: IgetAllLinesFittingWidth): {
+  color: string;
+  text: string;
+  type: "previous" | "current" | "next";
+}[] {
+  const allLines: {
+    color: string;
+    text: string;
+    type: "previous" | "current" | "next";
+  }[] = [];
+
+  lines.forEach((line, i) => {
+    const lineInfo = getLineInfo({
+      currentLine: line,
+      lyricLineColor,
+      lyricsProgressMs,
+      lyricTextColor,
+      nextLine: lines[i + 1],
+    });
+
+    const linesText = getLinesFittingCanvas(
+      ctx,
+      line.words ?? "",
+      (canvasWidth ?? 10) - 20
+    );
+    linesText.forEach((lineText) => {
+      allLines.push({
+        color: lineInfo.color,
+        text: lineText,
+        type: lineInfo.type,
+      });
+    });
+  });
+
+  return allLines;
 }
