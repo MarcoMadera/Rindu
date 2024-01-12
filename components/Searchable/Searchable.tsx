ceac71cc4c1289d5668c33edda5ef56fa130f52f
Marco Madera <me@marcomadera.com>
2024-01-12 12:21:26 -0700

    narrow type (#269)

--- components/Searchable/Searchable.tsx
+++ components/Searchable/Searchable.tsx
@@ -6,21 +6,23 @@ import { extractTextFromChildren } from "utils";
 export default function Searchable({
   searchTerm,
   children,
-}: PropsWithChildren<{ searchTerm: string }>): ReactElement | null {
-  const filteredChildren = Children.map(children, (child) => {
-    const text = extractTextFromChildren(child);
-    if (!searchTerm) return child;
+}: PropsWithChildren<{ searchTerm: string }>): ReactElement {
+  return (
+    <>
+      {Children.map(children, (child) => {
+        const text = extractTextFromChildren(child);
+        if (!searchTerm) return child;
 
-    if (
-      searchTerm &&
-      text &&
-      text.toLowerCase().includes(searchTerm.toLowerCase())
-    ) {
-      return <TextHighlighter text={searchTerm}>{child}</TextHighlighter>;
-    }
+        if (
+          searchTerm &&
+          text &&
+          text.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return <TextHighlighter text={searchTerm}>{child}</TextHighlighter>;
+        }
 
-    return null;
-  });
-
-  return <>{filteredChildren}</>;
+        return null;
+      })}
+    </>
+  );
 }
