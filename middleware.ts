e90b8a77d5352b60f540acaa6e7aa6bb1d2ee982
Marco Madera <me@marcomadera.com>
2024-03-25 15:04:19 -0700

    workaround (#291)

--- middleware.ts
+++ middleware.ts
@@ -8,27 +8,20 @@ import {
   parseAcceptLanguage,
 } from "utils";
 
-export function middleware(request: NextRequest): NextResponse {
+export function middleware(request: NextRequest): NextResponse | void {
   const acceptLanguage = request.headers.get("Accept-Language");
   const userLocales = parseAcceptLanguage(acceptLanguage ?? "");
   const locale =
     userLocales.find((loc) => Object.values(Locale).includes(loc as Locale)) ??
     DEFAULT_LOCALE;
-  const response = NextResponse.next();
 
   const localeCookie = request.cookies.get(LOCALE_COOKIE)?.value;
   const localeCookieValue = localeCookie ?? locale;
-  response.cookies.set(LOCALE_COOKIE, localeCookieValue, {
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 365,
-    path: "/",
-  });
-
-  request.cookies.set({
-    name: LOCALE_COOKIE,
-    value: localeCookieValue,
-  });
-
-  return response;
+  if (!localeCookie) {
+    // Have to redirect here to ensure cookie is available to root layout
+    // issue https://github.com/vercel/next.js/issues/49442
+    const response = NextResponse.redirect(request.url);
+    response.cookies.set(LOCALE_COOKIE, localeCookieValue);
+    return response;
+  }
 }
