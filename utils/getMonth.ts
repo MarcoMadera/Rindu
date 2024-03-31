599ed4f1a4a14620afffb58bcd3bbe6ab12731b2
Marco Madera <me@marcomadera.com>
2024-03-31 07:46:17 -0700

    unidentified (#306)

--- utils/getMonth.ts
+++ utils/getMonth.ts
@@ -1,5 +1,6 @@
 export function getMonth(month: number, locale: string): string {
   const date = new Date();
+  date.setDate(1);
   date.setMonth(month);
   return date.toLocaleString(locale, { month: "short" }).toUpperCase();
 }
