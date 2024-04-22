392f2f895fe6395d8bbe751aa0a896073d9ccca9
MarcoMadera <marcoseguro2@gmail.com>
2024-04-21 20:19:48 -0700

    html-to-react

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,6 +1,19 @@
-import { Meta } from "@storybook/react";
+import { ReactElement } from "react";
 
-import { Button } from "components";
+import { Meta } from "@storybook/react";
+import { sanitize } from "dompurify";
+
+import {
+  Button,
+  FollowButton,
+  LoginButton,
+  LyricsPIPButton,
+  Paragraph,
+  PlayButton,
+  RouterButtons,
+} from "components";
+import { htmlToReact, templateReplace } from "utils";
+import { Follow_type } from "utils/spotifyCalls";
 
 export default {
   title: "Components/Button",
@@ -13,5 +26,46 @@ export default {
 export const Default = {
   args: {
     children: "click me",
+  },
+};
+
+export const HtmlToReactButton = {
+  args: {
+    html: "",
+  },
+  render: ({ html }: { html: string }): ReactElement => {
+    const format = (content: string) =>
+      templateReplace(content, [
+        <Button
+          key="0"
+          onClick={() => {
+            alert("hola 0");
+          }}
+        >
+          This is your button 0
+        </Button>,
+        <LoginButton key="1" />,
+        <RouterButtons key="2" />,
+        <LyricsPIPButton key="3" />,
+        <PlayButton
+          key="4"
+          allTracks={[]}
+          centerSize={24}
+          size={52}
+          style={{ display: "inline" }}
+        />,
+        <FollowButton key="5" type={Follow_type.Artist} />,
+      ]);
+
+    return (
+      <>
+        <Paragraph>
+          {
+            "Include html in the controls and add {0} ... {5} to have different buttons"
+          }
+        </Paragraph>
+        <Paragraph>{htmlToReact(sanitize(html), format)}</Paragraph>
+      </>
+    );
   },
 };
