6b550ba2de2fe886c8b35d902081197ea7816578
MarcoMadera <me@marcomadera.com>
2025-02-27 10:35:17 -0700

    handle api error

--- utils/getArtistsOfTheWeek.ts
+++ utils/getArtistsOfTheWeek.ts
@@ -4,14 +4,26 @@ interface IArtistOfTheWeek {
       name: string;
     }[];
   };
+  error?: number;
+  message?: string;
 }
 export async function getArtistsOfTheWeek(
   apiKey: string
-): Promise<IArtistOfTheWeek> {
+): Promise<IArtistOfTheWeek | null> {
   const artistsOfTheWeekRes = await fetch(
     `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json`
   );
   const artistsOfTheWeek =
     (await artistsOfTheWeekRes.json()) as IArtistOfTheWeek;
+
+  if (artistsOfTheWeek.error) {
+    console.error(`${artistsOfTheWeek.error}: ${artistsOfTheWeek.message}`);
+    return null;
+  }
+
+  if (!artistsOfTheWeekRes.ok) {
+    return null;
+  }
+
   return artistsOfTheWeek;
 }
--- utils/getTracksOfTheWeek.ts
+++ utils/getTracksOfTheWeek.ts
@@ -7,14 +7,25 @@ interface ITracksOfTheWeek {
       };
     }[];
   };
+  error?: number;
+  message?: string;
 }
 
 export async function getTracksOfTheWeek(
   apiKey: string
-): Promise<ITracksOfTheWeek> {
+): Promise<ITracksOfTheWeek | null> {
   const tracksOfTheWeekRes = await fetch(
     `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`
   );
   const tracksOfTheWeek = (await tracksOfTheWeekRes.json()) as ITracksOfTheWeek;
+  if (tracksOfTheWeek.error) {
+    console.error(`${tracksOfTheWeek.error}: ${tracksOfTheWeek.message}`);
+    return null;
+  }
+
+  if (!tracksOfTheWeekRes.ok) {
+    return null;
+  }
+
   return tracksOfTheWeek;
 }
