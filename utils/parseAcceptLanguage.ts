1862147348534fa6664530f951fcfc85453b9434
MarcoMadera <marcoseguro2@gmail.com>
2025-03-01 19:17:27 -0700

    langs

--- utils/parseAcceptLanguage.ts
+++ utils/parseAcceptLanguage.ts
@@ -1,8 +1,12 @@
 export function parseAcceptLanguage(acceptLanguage = ""): string[] {
   const languages = acceptLanguage.split(",").map((lang) => {
     const [code, q = "1"] = lang.trim().split(";q=");
-    return { code: code.trim(), q: parseFloat(q) };
+    const baseCode = code.split("-")[0];
+    return { code: baseCode.trim(), q: parseFloat(q) };
   });
 
-  return languages.toSorted((a, b) => b.q - a.q).map(({ code }) => code);
+  return languages
+    .toSorted((a, b) => b.q - a.q)
+    .map(({ code }) => code)
+    .filter((code, index, self) => self.indexOf(code) === index);
 }
