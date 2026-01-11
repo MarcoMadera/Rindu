81f0ce1b825a55dc418f173abfdf9c6fcaed1e36
Marco Madera <me@marcomadera.com>
2026-01-10 18:55:12 -0700

    m (#529)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import { ErrorBoundary, Seo } from "components";
 import { AppContextProvider } from "context/AppContextProvider";
 import { TranslationsContextProviderValue } from "context/TranslationsContext";
 import MainLayout from "layouts/MainLayout";
+import { DEFAULT_LOCALE, loadLocalization } from "utils";
 
 interface AppContextProviderProps extends TranslationsContextProviderValue {
   user: SpotifyApi.UserObjectPrivate | null;
@@ -17,15 +18,16 @@ const MyApp = ({
   Component,
   pageProps,
 }: AppProps<AppContextProviderProps>): ReactElement => {
+  const translations = loadLocalization(DEFAULT_LOCALE);
   return (
     <ErrorBoundary>
       <AppContextProvider
-        translations={pageProps.translations}
+        translations={pageProps.translations ?? translations}
         locale={pageProps.locale}
         userValue={{ user: pageProps.user }}
       >
         <Seo />
-        <MainLayout translations={pageProps.translations}>
+        <MainLayout translations={pageProps.translations ?? translations}>
           <Component {...pageProps} />
           <Analytics />
         </MainLayout>
